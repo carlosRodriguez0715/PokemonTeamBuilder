bb8b45a2eb14c1fcc81249871c54fa08bf6944c6
Carlos Rodriguez <CarlosR@10.0.0.131>
2023-03-06 13:57:06 -0500

    Controller class modified, fxml add remove buttons ids added

--- PokeTeamBuilder/src/application/TBuilderController.java
+++ PokeTeamBuilder/src/application/TBuilderController.java
@@ -1,5 +1,12 @@
 package application;
 
+/**
+ * @author: Carlos Rodriguez
+ * Class takes care of the actions from the FXML file*/
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class TBuilderController {
-	
+	@FXML
+	private Button slot1,slot2,slot3,slot4,slot5,slot6, addBtn, removeBtn;
 }
