3747862de693bdada01d727da839d89b8a376230
Carlos Rodriguez <CarlosR@10.0.0.131>
2023-03-06 14:35:39 -0500

    Messing aorund with HTTP package, gson jar added to convert to JSON and viceversa

--- PokeTeamBuilder/src/application/TBuilderController.java
+++ PokeTeamBuilder/src/application/TBuilderController.java
@@ -4,15 +4,50 @@ package application;
  * @author: Carlos Rodriguez
  * Class takes care of the actions from the FXML file*/
 import javafx.fxml.FXML;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public class TBuilderController {
 	@FXML
 	private Button slot1,slot2,slot3,slot4,slot5,slot6, addBtn, removeBtn;
 	
 	@FXML
-	public void search() {}
+	private TextField searchBar;
 	
 	@FXML
-	public void remove() {}
+	private TextArea printArea;
+	
+	@FXML
+	public void search() {
+		String textFound = searchBar.getText();
+		if(textFound.isBlank()) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setContentText("Search bar is empty, please enter a valid Pokemon!");
+			alert.showAndWait();
+		}
+		else {
+			textFound = textFound.toLowerCase();
+			try {
+				URL url = new URL("https://pokeapi.co/api/v2/pokemon/" + textFound);
+				HttpURLConnection con = (HttpURLConnection) url.openConnection();
+				con.setRequestMethod("GET");
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@FXML
+	public void remove () {}
 }
